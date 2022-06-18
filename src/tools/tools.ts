57c26f392b479c10120fb0abf07776316888a139
ctrlj-dev <jesuscc1701@gmail.com>
2022-06-18 12:46:20 +0200

    Refactor: Docs on tools.ts

--- src/tools/tools.ts
+++ src/tools/tools.ts
@@ -1,13 +1,32 @@
 import IProduct from "models/IProduct";
 import { ESortProductsByPrice } from "models/IProductsFilters";
 
+/**
+ * Calculate discount on products
+ * @param price number ,
+ * @param discount number 
+ */
 export const calculateDiscount = (price: number, discount: number): number => price - (price * discount / 100);
 
-export const roundToTwo = (number: number): number => Math.round(number * 100) / 100
+/**
+ * Round number to two decimals.
+ * @param price number 
+*/
+export const roundToTwo = (price: number): price => Math.round(price * 100) / 100
 
+/**
+ * Checke if discount exist as a object property, and apply it.
+ * @param product IProduct 
+*/
 export const checkDiscountExist = (product: IProduct): number => product.discount ? calculateDiscount(product.price, product.discount) : product.price
 
-export const orderProducts = (a: IProduct, b: IProduct, option: string): number => {
+/**
+ * Sort products by price
+ * @param a IProduct
+ * @param b IProduct
+ * @param option string
+*/
+export const orderProductsByPrice = (a: IProduct, b: IProduct, option: string): number => {
     switch (option) {
         case ESortProductsByPrice.DESC:
             return checkDiscountExist(a) > checkDiscountExist(b) ? -1 : 1
@@ -18,9 +37,15 @@ export const orderProducts = (a: IProduct, b: IProduct, option: string): number 
     }
 }
 
+/**
+ * Function to filter products by search and sort by pricefilter
+ * @param searchTerms string ,
+ * @param data IProduct[] ,
+ * @param sortOptions string ,
+*/
 export const dataSortSearchResults = (searchTerms: string, data: IProduct[], sortOptions: string) => !searchTerms ? data.concat().sort((a: IProduct, b: IProduct) =>
-    orderProducts(a, b, sortOptions)
+    orderProductsByPrice(a, b, sortOptions)
 ) :
     data.filter(product =>
         product.name.toLowerCase().includes(searchTerms.toLocaleLowerCase())
-    ).concat().sort((a, b) => orderProducts(a, b, sortOptions))
+    ).concat().sort((a, b) => orderProductsByPrice(a, b, sortOptions))
