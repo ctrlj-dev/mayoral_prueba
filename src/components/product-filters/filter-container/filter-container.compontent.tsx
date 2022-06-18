af69047a70c502d86a3d88f33c82892cdaec4117
ctrlj-dev <jesuscc1701@gmail.com>
2022-06-19 00:01:15 +0200

    Feature: New container componten for filters.

---
+++ src/components/product-filters/filter-container/filter-container.compontent.tsx
@@ -0,0 +1,17 @@
+import { FC } from 'react';
+import styles from './filter-container.module.scss';
+
+interface Props {
+    className?: string
+    children: JSX.Element[],
+}
+
+const FilterContainer: FC<Props> = (props) => {
+    const { className, children } = props
+    return (
+        <div className={`${styles.filterRow} ${className} row`}>
+                {children}
+        </div>
+    )
+}
+export default FilterContainer
