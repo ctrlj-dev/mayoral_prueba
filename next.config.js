4d9a89c4f05900aa57c079941b22b2b5f9ed09c0
ctrlj-dev <jesuscc1701@gmail.com>
2022-06-18 18:43:41 +0200

    Feature: Add variables.scss styles globally. Refactor: Delete manually scss importation.

--- next.config.js
+++ next.config.js
@@ -8,6 +8,7 @@ module.exports = {
     domains: ['json.ctrlj.es'],
   },
   env: { BASE_URL: process.env.BASE_URL },
+  sassOptions: {
+    additionalData: `@import "src/styles/variable.scss";`,
+  },
 };
-
-
