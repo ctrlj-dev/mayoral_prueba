145a5ab3bdadf2fb642e8cba702f918422163be1
ctrlj-dev <jesuscc1701@gmail.com>
2022-06-19 09:40:45 +0200

    Feature: Add test to filter container

---
+++ src/components/product-filters/filter-container/filter-container.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react';
+import FilterContainer from './filter-container.component';
+
+describe('FilterContainer specs', () => {
+    test('test to check if container display his children properly', () => {
+        //Arrange
+        //Act
+
+        const screen = render(<FilterContainer><p>filter1</p><p>filter2</p></FilterContainer>)
+
+        const children1 = screen.getByText('filter1');
+        const children2 = screen.getByText('filter2');
+
+        expect(children1).not.toBeNull()
+        expect(children2).not.toBeNull()
+
+        expect(screen.asFragment()).toMatchSnapshot();
+
+        //Assert
+    })
+})
+
+
