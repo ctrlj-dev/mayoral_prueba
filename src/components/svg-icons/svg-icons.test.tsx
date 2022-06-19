4c35b1493788ec9befc790b4c0c2ecf6c6d1cf73
ctrlj-dev <jesuscc1701@gmail.com>
2022-06-19 10:22:15 +0200

    Feature: Add test for svg icons.

---
+++ src/components/svg-icons/svg-icons.test.tsx
@@ -0,0 +1,54 @@
+import { getAllByRole, render } from '@testing-library/react';
+import { SearchIcon, LineIcon, CrossIcon } from './svg-icons.component';
+
+describe('SVG Icons specs', () => {
+    test('Test for Icons to check if are render properly', () => {
+        //Act
+        const screen = render(
+            <>
+                <div data-testid="searchIcon">
+                    <SearchIcon
+                        id={"searchIcon"}
+                        className={'searchIcon'}
+                        width={20}
+                        height={20}
+                    />
+                </div>
+                <div data-testid="lineIcon">
+                    <LineIcon
+                        id={"lineIcon"}
+                        className={'lineIcon'}
+                        width={20}
+                        height={20}
+                    />
+                </div>
+                <div data-testid="crossIcon">
+                    <CrossIcon
+                        id={"crossIcon"}
+                        className={'crossIcon'}
+                        width={20}
+                        height={20}
+                    />
+                </div>
+            </>
+        )
+
+        const searchIcon = screen.getByTestId('searchIcon').firstChild.nodeName;
+        const lineIcon = screen.getByTestId('lineIcon').firstChild.nodeName;
+        const crossIcon = screen.getByTestId('crossIcon').firstChild.nodeName;
+        //Assert
+        expect(searchIcon).not.toBeNull();
+        expect(searchIcon).toEqual('svg')
+        expect(screen.asFragment()).toMatchSnapshot();
+
+        expect(lineIcon).not.toBeNull();
+        expect(lineIcon).toEqual('svg')
+        expect(screen.asFragment()).toMatchSnapshot();
+
+        expect(crossIcon).not.toBeNull();
+        expect(crossIcon).toEqual('svg')
+        expect(screen.asFragment()).toMatchSnapshot();
+
+
+    })
+})
